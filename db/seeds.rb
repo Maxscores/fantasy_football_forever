b1f6ecd4a3d36486b64dcbaa8b20da3fdd83367a
maxscores <maxstackhouse@gmail.com>
2017-12-18 10:40:54 -0700

    Update Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,7 @@ wr = Player.where(position: "WR")
 te = Player.where(position: "TE")
 
 seasons = [2010, 2011, 2012, 2013, 2014]
-qb_completions = normal_distribution(qb.count * seasons.count, 2800, 2)
+qb_completions = normal_distribution(qb.count * seasons.count, 300, 30)
 qb_completion_percentages = normal_distribution(qb.count * seasons.count, 65, 2)
 qb_passing_yards = normal_distribution(qb.count * seasons.count, 3500, 5)
 qb_passing_touchdowns = normal_distribution(qb.count * seasons.count, 35, 8)
