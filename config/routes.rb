f254caa31fd102c9d073e0f2831dab8539f6f69a
maxscores <maxstackhouse@gmail.com>
2017-12-18 13:36:09 -0700

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   resources :fantasy_teams
   resources :players do
     resources :season_stats
   end
+  resources :users, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
