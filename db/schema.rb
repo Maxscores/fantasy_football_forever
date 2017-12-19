ae3968e43a4c2bef9ec9bc9c31208cf4a2b542f0
maxscores <maxstackhouse@gmail.com>
2017-12-19 10:17:15 -0700

    Add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218213731) do
+ActiveRecord::Schema.define(version: 20171219170740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20171218213731) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.string "role"
     t.string "full_name"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "players", "fantasy_teams"
