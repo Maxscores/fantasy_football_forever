0cf149b2dc08084dbe5f03ea4726b4e3f6267962
maxscores <maxstackhouse@gmail.com>
2017-12-19 21:45:43 -0700

    remove favorite player from my dashboard

--- app/controllers/user_favorites_controller.rb
+++ app/controllers/user_favorites_controller.rb
@@ -9,6 +9,10 @@ class UserFavoritesController < ApplicationController
   def destroy
     favorite = UserFavorite.find_by(player_id: params[:player_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to players_path({:page => params[:page], :team => params[:team], :position => params[:position]})
+    if params[:view] == "show"
+      redirect_to user_path(current_user)
+    else
+      redirect_to players_path({:page => params[:page], :team => params[:team], :position => params[:position]})
+    end
   end
 end
